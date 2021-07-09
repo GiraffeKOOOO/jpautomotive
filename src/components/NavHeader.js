8eb2e09912d00ff08af4004e962fd695f5be875e
GiraffeKOOOO <s5074177@bournemouth.ac.uk>
2021-07-09 18:00:49 +0100

    removed the nav buttons for now, centralised the information, still need to center the contact hours

--- src/components/NavHeader.js
+++ src/components/NavHeader.js
@@ -22,44 +22,28 @@ function NavHeader() {
                             id="logo-header"
                         />
                     </Col>
-                    <Col id="nav-buttons">
-                        <Container id="container-right">
-                            <Row id="row-top">
-                                {/*information about business here*/}
-                                <div className="info-div">
-                                    <table id="contact-hours-table">
-                                        <tbody>
-                                            <tr>
-                                                <td>Contact hours</td>
-                                                <td>Monday-Friday</td>
-                                            </tr>
-                                            <tr>
-                                                <td id="clock-svg"><ClockFill color="black" size={19}/></td>
-                                                <td id="top-line">9am-5pm</td>
-                                            </tr>
-                                        </tbody>
-                                    </table>
-                                </div>
-                                <div className="info-div">
-                                    <TelephoneFill color="black" size={19}/><p>07498 943120</p>
-                                </div>
-                                <div className="info-div">
-                                    <EnvelopeFill color="black" size={19}/><p>j.pautomotivetec@gmail.com</p>
-                                </div>
-                            </Row>
-                            <Row id="row-bottom">
-                                {/* whilst this is hosted on github pages, the path for the homepage needs to include jpautomotive*/}
-                                <Link to='/jpautomotive'>
-                                    <Button theme="secondary" themeType="contained" className="nav-button">Home</Button>
-                                </Link>
-                                <Link to='/jpautomotive/about'>
-                                    <Button theme="secondary" themeType="contained" className="nav-button">About</Button>
-                                </Link>
-                                <Link to='/jpautomotive/contact'>
-                                    <Button theme="secondary" themeType="contained" className="nav-button">Contact Us</Button>
-                                </Link>
-                            </Row>
-                        </Container>
+                    <Col id="nav-info">
+                        {/*information about business here*/}
+                        <div className="info-div" id="contact-hours-tbl">
+                            <ClockFill color="black" size={21}/>
+                            <table>
+                                <tbody>
+                                    <tr>
+                                        <td>Contact Hours</td>
+                                    </tr>
+                                    <tr>
+                                        <td>Mon-Fri</td>
+                                        <td>9am-5pm</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <div className="info-div">
+                            <TelephoneFill color="black" size={22}/><p>07498943120</p>
+                        </div>
+                        <div className="info-div">
+                            <EnvelopeFill color="black" size={22}/><p>j.pautomotivetec@gmail.com</p>
+                        </div>
                     </Col>
                 </Row>
             </Container>
