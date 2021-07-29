54cfbeead148daa1f55a9005cd23328681cbf91f
GiraffeKOOOO <pstepien@hotmail.co.uk>
2021-07-29 01:59:56 +0100

    quick change to tracker component
    removed the state, turns out the number doesn't have to be a state variable

--- src/components/Tracker.js
+++ src/components/Tracker.js
@@ -1,14 +1,14 @@
 // import react
 import React from 'react';
-import { useState } from 'react';
 // import necessary components
 import AnimatedNumber from "animated-number-react";
 // import styling
 import '../css/components/Tracker.css';
 
 function Tracker(props) {
-    const [value, setValue] = useState(props.trackerNumber);
+    const value = props.trackerNumber;
     const formatValue = value => `${Number(value).toFixed(0)}`;
+
     return (
         <div id="tracker-container">
             <AnimatedNumber
